577899528e692fb826cd5fce027e9fa7ccf705d4
Zhiyang He <hezhiyang@mail.ustc.edu.cn>
2022-09-17 22:53:32 +0800

    添加了文档开头的模板

--- lua/luasnip-latex-snippets/normal_wA.lua
+++ lua/luasnip-latex-snippets/normal_wA.lua
@@ -6,6 +6,20 @@ local normal_wA = {
     { trig = "dm", name = "Block Math" },
     "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\] $0"
   ),
+  ls.parser.parse_snippet(
+    {trig="template", name = "Template"},
+      [[
+        %! TEX TS-program = xelatex
+        \PassOptionsToPackage{quiet}{fontspec}
+        \documentclass[utf8]{ctexart}
+        \usepackage{amsmath, amssymb}
+        \usepackage[]{fontspec}
+
+        \\begin{document}
+          $0
+        \\end{document}
+      ]]
+    )
 }
 
 return normal_wA
